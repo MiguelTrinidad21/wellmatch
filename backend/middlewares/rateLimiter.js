1adc805f8afc09628cf8a2f5d5dc93db3c445701
Alessandro Miguel Trinidad <migueltrinidad0621@gmail.com>
2026-09-24 17:07:38 +0800

    fix: Skip successful api calls from incrementing attempts in rate limit

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -3,27 +3,30 @@ import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 5,                          
+    limit: 5,                          
     standardHeaders: true,
     legacyHeaders: false,
+    skipSuccessfulRequests: true,
     message: { message: "Too many attempts. Please try again in 15 minutes." }
 });
 
 
 export const generalLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 10,                          
+    limit: 10,                          
     standardHeaders: true,
     legacyHeaders: false,
+    skipSuccessfulRequests: true,
     message: { message: "Too many attempts. Please try again in 15 minutes." }
 });
 
 
 export const passwordResetLimiter = rateLimit({
     windowMs: 60 * 60 * 1000,
-    max: 5,                          
+    limit: 5,                          
     standardHeaders: true,
     legacyHeaders: false,
+    skipSuccessfulRequests: true,
     message: { message: "Too many attempts. Please try again after an hour." }
 });
 
